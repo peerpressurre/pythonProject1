9a621b7a1169fd573807301b14bd9775b64bcc5f
peerpressurre <gsophiay@gmail.com>
2022-09-25 13:37:34 +0300

    Update: feature/hm(3)_ex_2

--- main.py
+++ main.py
@@ -2,12 +2,27 @@ num1 = int(input('enter first num->'))
 num2 = int(input('enter second num->'))
 num3 = int(input('enter third num->'))
 
-print('| + -> sum\n| * -> mul')
+print('| min -> minimum\n| max -> maximum\n| avr -> average ')
 op = input('->')
 res = ''
-if op == '+':
-    res = num1+num2+num3
-elif op == '*':
-    res = num1*num2*num3
-
+if op == 'min':
+    if num1 < num2 and num1 < num3:
+        res = num1
+    elif num2 < num1 and num2 < num3:
+        res = num2
+    elif num3 < num1 and num3 < num2:
+        res = num3
+    else:
+        res = 'the numbers are equal'
+elif op == 'max':
+    if num1 > num2 and num1 > num3:
+        res = num1
+    elif num2 > num1 and num2 > num3:
+        res = num2
+    elif num3 > num1 and num3 > num2:
+        res = num3
+    else:
+        res = ' the numbers are equal'
+elif op == 'avr':
+    res = (num1+num2+num3)/3
 print(f'res = {res}')
