d31f8379da1e3e2c501c7dd33c3f50cd9d012bc6
peerpressurre <gsophiay@gmail.com>
2022-09-25 13:20:51 +0300

    Update: feature/hm(2)_ex_5

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-length = int(input('length->'))
-height = int(input('height->'))
-print(float((length*height)*0.5))
+number = int(input('number->')) #->1234
+x1 = number%10 #123->4
+number=int(number/10)
+x2 = number%10 #12->3
+number=int(number/10)
+x3 = number%10 #1->2
+number=int(number/10)
+x4 = number #1
+res = int(x1*1000+x2*100+x3*10+x4)
+
+print(res)
